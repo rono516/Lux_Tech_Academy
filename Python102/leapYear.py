1d421f02aac61ffa2407f04b74c848fc0ebf31c7
rono516 <ronocollins2000@gmail.com>
2022-02-20 15:37:18 +0300

    leapYear

--- Python102/leapYear.py
+++ Python102/leapYear.py
@@ -1,9 +1,23 @@
-#year = int(input("Enter year: "))
 
-def isLeap(year):
-    if year%4 == 0 and year%100 == 0:
-        return (f' {year} is a leap year')
-    else   :
-        return(f'{year} is not a leap year')
+#This is a python program to check if a year is a leap year
+#a leap year is divisible by 4 and by 400 if it ends with two zeros e.g 2000
 
-print(isLeap(2015))
+
+
+year = int(input("Enter year to check: "))
+LeapYear = year%4
+LeapCentury = year%100
+Leap2Century = year%400
+
+
+if LeapYear == 0 and LeapCentury != 0:
+      
+  print(f"{year} is a Leap Year")
+
+elif LeapCentury == 0 and Leap2Century == 0:
+
+  print(f"{year} is a Leap Centuary Year" )
+
+else:
+
+  print(f"{year} is not a Leap Year")
